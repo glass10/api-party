99266c3a81976d6a0c31b1e18bf939e250e4fc3d
Cameron Glass <glass10@purdue.edu>
2017-06-20 15:31:47 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter as Router } from 'react-route-dom'
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <div className="App-headings">
+            
+          </div>
+          </div>
+      </div>
+    );
+  }
+}
+
+export default App;
